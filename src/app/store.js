fcff962c05589070311db15ffa8780715914c704
Karim Ayman <nighthawk1486@gmail.com>
2021-10-05 20:22:00 +0200

    Adding the final authedUser reducer to the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import usersReducer from '../slices/users'
 import questionsReducer from '../slices/questions'
+import authedUserReducer from '../slices/authedUser'
 import logger from 'redux-logger'
 
 export const store = configureStore({
     reducer: {
+        authedUser: authedUserReducer,
         users: usersReducer,
         questions: questionsReducer,
     },
