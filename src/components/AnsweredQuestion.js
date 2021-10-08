eddd9242c73872230b9191a75a0f0e5a63d4be6f
Karim Ayman <nighthawk1486@gmail.com>
2021-10-08 18:02:17 +0200

    Implementing the AnsweredQuestion component

--- src/components/AnsweredQuestion.js
+++ src/components/AnsweredQuestion.js
@@ -7,10 +7,12 @@ import Box from '@mui/material/Box';
 import LinearProgress from '@mui/material/LinearProgress';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectAuthedUser } from '../slices/authedUser'
+import { selectUsers } from '../slices/users'
+import { selectQuestions } from '../slices/questions'
 
 function LinearProgressWithLabel(props) {
-    const {id} = useParams()
-
     return (
       <Box sx={{ display: 'flex', alignItems: 'center' }}>
         <Box sx={{ width: '100%', mr: 1 }}>
@@ -20,32 +22,43 @@ function LinearProgressWithLabel(props) {
           <Typography variant="body2" color="text.secondary">{`${Math.round(
             props.value,
           )}%`}</Typography>
-          <CheckCircleOutlineIcon />
+          {props.selected === props.option &&
+          <CheckCircleOutlineIcon />}
         </Box>
       </Box>
     );
   }
 
 export default function ActionAreaCard() {
+    const {id} = useParams()
+    const authedUser = useSelector(selectAuthedUser)
+    const users = useSelector(selectUsers)
+    const questions = useSelector(selectQuestions)
+    const optionOneVotes = questions[id].optionOne.votes.length
+    const optionTwoVotes = questions[id].optionTwo.votes.length
+    const totalVotes = optionOneVotes + optionTwoVotes
+    const selectedOption = users[authedUser].answers[id]
+
   return (
     <Card className='answered-question'>
         <CardContent>
           <Typography gutterBottom variant="h" component="div" className='user-asked'>
-            User
+            Asked by {users[questions[id].author].name}
           </Typography>
           <Box className = 'user-question'>
-            <Avatar alt="User" src="" className='avatar'/>
+            <Avatar alt={users[questions[id].author].name} src={users[questions[id].author].avatarURL} className='avatar'/>
             <Box className='options'>
                 <Typography className='results'>Results:</Typography>
                 <Box className='container'>
-                    <Typography>Option One</Typography>
-                    <LinearProgressWithLabel value={10} />
-                    <Typography>Vote percentage</Typography>
+                    <Typography sx={{fontWeight:600, marginBottom: 1}}>{questions[id].optionOne.text}</Typography>
+                    <LinearProgressWithLabel value={(optionOneVotes/totalVotes*100)} option='optionOne' selected={selectedOption} />
+                    <Typography sx={{marginTop:1}}>{optionOneVotes} out of {totalVotes} votes</Typography>
                 </Box>
                 <Box className='container'>
-                    <Typography>Option Two</Typography>
-                    <LinearProgressWithLabel value={20} />
-                    <Typography>Vote percentage</Typography>
+                    <Typography sx={{fontWeight:600, marginBottom: 1}}>{questions[id].optionTwo.text}</Typography>
+                    <LinearProgressWithLabel value={(optionTwoVotes/totalVotes*100)} option='optionTwo' selected={selectedOption}/>
+                    <Typography sx={{marginTop:1}}>{optionTwoVotes} out of {totalVotes} votes</Typography>
+                    
                 </Box>
             </Box>
           </Box>
